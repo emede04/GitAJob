1493a9bc09d3a1f77a7e65c26478bf589149ba44
Anomie <55450208+mariadolores1@users.noreply.github.com>
2022-02-26 20:02:07 +0100

    logica de bd; funcionando

---
+++ app/src/main/java/com/example/gitajob/activity/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.example.gitajob.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.gitajob.R;
+import com.example.gitajob.basededatos.Db_Master;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText user;
+    EditText password;
+    Db_Master bd;
+    Button bLogin;
+    Button bSignUp;
+
+    int bdV;
+
+
+    @Override
+    protected void onCreate(Bundle saved) {
+        super.onCreate(null); // <-- por ahora esta en null porque quiero que se reisntale todo el rato
+        setContentView(R.layout.activity_login);
+        initComponents(user, password, bd, bLogin, bSignUp);
+        cargarImagenes();
+        bd = new Db_Master(this);
+
+        //creo mi base de datos
+        bdV = bd.getVersionDB(); //paso mi numero de version;
+
+        //le doy funcionalidad a los botones
+        bSignUp.setOnClickListener(this::onClick);
+        bLogin.setOnClickListener(this::onClick);
+        //un array inicializador de botones, que guay
+        // for (Button button : Arrays.asList(bLogin, bSignUp)) {
+        //            button.setOnClickListener(this);
+        //        }
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {    //metodo para poner todos los lisenter de la de golpe
+
+        switch (v.getId()/*to get clicked view id**/) {
+            case R.id.bLogin:
+
+                boolean kapasao=   bd.verifyUserData(user.getText().toString(), password.getText().toString());
+                if(kapasao){
+                    Toast.makeText(this, "Pa dentro", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+
+                    startActivity(intent);
+                    finish();
+
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("los datos son incorrectos")
+                            .setTitle("Error")
+                            .setIcon(android.R.drawable.ic_notification_clear_all);
+                    builder.show();
+
+                }
+
+
+                break;
+            case R.id.bRegistrar:
+                String texto1 = user.getText().toString();
+                String texto2 = user.getText().toString();
+                if ((texto1 == "") || (texto2 == "")) {
+                    Toast.makeText(this, "Has de rellenar todos los campos1", Toast.LENGTH_LONG + 2).show();
+                    user.setText("");
+                    password.setText("");
+                    break;
+                } else {
+                        kapasao = bd.verifyUserData(texto1,texto2);
+                    if (kapasao){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage("Ya existen los datos que desea registrar")
+                                .setTitle("Error")
+                                .setIcon(android.R.drawable.ic_delete);
+                        builder.show();
+                    } else {
+                        bd.insertUsuarios(user.getText().toString(), password.getText().toString());
+                        Toast.makeText(this, "bd", Toast.LENGTH_LONG + 2).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+
+
+                }
+                }
+            default:
+                break;
+        }
+    }
+
+    public void cargarImagenes() {
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(LoginActivity.this)
+                //esto es una biblioteca donde subir icones svg
+                .load("https://cdn4.iconfinder.com/data/icons/bubble-gradient-social-media-1/200/steam-512.png").centerCrop()
+                .placeholder(android.R.drawable.ic_menu_edit)
+                .error(R.drawable.ic_launcher_foreground)
+                .into(imageView);
+
+        ImageView stack = (ImageView) findViewById(R.id.stack);
+        Glide.with(LoginActivity.this)
+                .load("https://cdn1.iconfinder.com/data/icons/apps-8/64/stack-overflow-stackoverflow-apps-platform-512.png")
+                .placeholder(android.R.drawable.ic_delete)
+                .into(stack);
+        // https://cdn0.iconfinder.com/data/icons/social-media-flags/398/Flag_Social_Media_Icons-29-256.png
+
+
+    }
+
+    public void initComponents(EditText user, EditText password, Db_Master bd, Button bLogin, Button bSignUp) {
+        this.user = (EditText) findViewById(R.id.txtUsuario);
+        this.password = (EditText) findViewById(R.id.txtContrasenia);
+        this.bLogin = (Button) findViewById(R.id.bLogin);
+        this.bSignUp = (Button) findViewById(R.id.bRegistrar);
+
+
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/example/gitajob/basededatos/Db_Master.java
@@ -0,0 +1,105 @@
+package com.example.gitajob.basededatos;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.example.gitajob.activity.LoginActivity;
+
+public class Db_Master extends SQLiteOpenHelper {
+    private static final String DB_NAME = "GitAJob";
+
+    //Table name
+    private static final String DB_TABLE_NAME = "USERS";
+
+    //Database version must be >= 1
+    private static final int DB_VERSION = 1;
+
+    //Columns
+    private static final String USER_NAME_COLUMN = "CUSER";
+
+    private static final String USER_PASSWORD_COLUMN = "CPASS";
+
+
+    private Context miContexto;
+
+    public Db_Master(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+        miContexto = context;
+
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_USER_TABLE = "CREATE TABLE " + DB_TABLE_NAME + "("
+                + USER_NAME_COLUMN + " TEXT ," + USER_PASSWORD_COLUMN + " TEXT " + ")";
+
+        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+        Log.d("base de datos","tablas creadas");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+        //para obtener las version de la base de datos
+    public int getVersionDB() {
+        int version = this.getReadableDatabase().getVersion();
+        return version;
+
+    }
+
+    public long insertUsuarios(String name, String pass) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = -1;
+
+        // Contenedor clave,valor -> columna, valor de entrada registro
+        ContentValues values = new ContentValues();
+
+        values.put(USER_NAME_COLUMN, name);
+        values.put(USER_PASSWORD_COLUMN, pass);
+        Log.d("base de datos","usuario creado");
+        Toast.makeText(miContexto, "USUARIO CREADO", Toast.LENGTH_LONG + 2).show();
+         result = db.insert(DB_TABLE_NAME, null, values);
+
+        //cerramos las conexion
+        db.close();
+
+        return result;
+
+    }
+
+
+    public Boolean verifyUserData(String user, String password) {
+        //comprueba primero si existe un registro con usuario.
+        SQLiteDatabase db = this.getWritableDatabase();            //importante dejar espacio entre las comillas que si no da error
+        Cursor cursorpass;
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DB_TABLE_NAME + " WHERE " + USER_NAME_COLUMN + "= ?", new String[]{user});
+        if (cursor.getCount() >= 1) {
+            cursorpass = db.rawQuery("SELECT '" + user + "' FROM " + DB_TABLE_NAME + " WHERE " + USER_PASSWORD_COLUMN + " = ? ", new String[]{password});
+            if (cursorpass.getCount() >=1) {
+                Log.d("base de datos","el usuario existe, entra, o se ha pulsado el boton de registrar por tanto no hace nada");
+                return true;
+            } else {
+                Log.d("base de datos" , "usuario existe, pero la contesañe es incorrecta");
+                return false;
+            }
+        } else {
+
+            Log.d("base de datos","el usuario no existe");
+            return  false;
+        }
+    }
+
+}
