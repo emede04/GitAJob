c30c7ae30242598d80ad74d707d44287042f46bf
Anomie <55450208+mariadolores1@users.noreply.github.com>
2022-02-27 00:56:48 +0100

    me duele el cuello y me estoy peleando con el recycle view, cambio y corto y se me ha cambiado la cuenta de github en fin

--- app/src/main/java/com/example/gitajob/activity/MainActivity.java
+++ app/src/main/java/com/example/gitajob/activity/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.gitajob.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.gitajob.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
